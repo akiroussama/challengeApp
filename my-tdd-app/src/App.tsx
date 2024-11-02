79de2000d3373616edbc919dee884d6b28b1b2f3
Akir Oussama <akir.oussama@gmail.com>
2024-11-02 10:10:53 +0100

    add app

---
+++ my-tdd-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the TDD Challenge</h1>
+      {/* Students will add more code here */}
+    </div>
+  );
+}
+
+export default App;
